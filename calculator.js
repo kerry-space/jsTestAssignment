1ebf887fe194f4414125fd17f5fc546029d13adc
António Quaresma <antqua@outlook.pt>
2023-05-03 16:48:10 +0100

    update the caeserCypher function and tests

--- calculator.js
+++ calculator.js
@@ -1,21 +1,37 @@
 
-function add(a, b){
-    return a + b;
-}
+// function add(a, b){
+//     return a + b;
+// }
 
 
-function subtract(a,b){
-    return a - b;
-}
+// function subtract(a,b){
+//     return a - b;
+// }
 
 
-function divide(a,b){
-    return a / b;
-}
+// function divide(a,b){
+//     return a / b;
+// }
 
 
-function multiply(a, b){
-    return a * b;
-}
+// function multiply(a, b){
+//     return a * b;
+// }
 
-export {add, subtract, divide, multiply}
+
+
+//export {add, subtract, divide, multiply}
+
+const calculator = {
+    add: (num1, num2) => num1 + num2,
+    subtract: (num1, num2) => num1 - num2,
+    multiply: (num1, num2) => num1 * num2,
+    divide: (num1, num2) => {
+      if (num2 === 0) {
+        throw new Error('Division by zero');
+      }
+      return num1 / num2;
+    },
+  };
+
+  export { calculator }
--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,7 @@
 import { capitalize } from "../capitalize"
 import {reverseString} from "../reverseString"
-import {add, subtract, divide, multiply} from "../calculator"
+//import {add, subtract, divide, multiply} from "../calculator"
+import {calculator} from "../calculator"
 import {caesarCipher} from "../caesarCipher"
 
 // test('capitalize function capitalizes the first letter of a string', () => {
@@ -60,29 +61,51 @@ describe('reverseString', () => {
     });
   });
 
-  
-test('add a + b  function', () => {
-  expect(add(2,2)).toEqual(4);
-});
 
-test('subtract a - b  function', () => {
-    expect(subtract(2,2)).toEqual(0);
-});
+// test('add a + b  function', () => {
+//   expect(add(2,2)).toEqual(4);
+// });
 
-
-test('divide a / b  function', () => {
-    expect(divide(2,2)).toEqual(1);
-});
+// test('subtract a - b  function', () => {
+//     expect(subtract(2,2)).toEqual(0);
+// });
 
 
-test('multiply a * b  function', () => {
-    expect(multiply(2,2)).toEqual(4);
+// test('divide a / b  function', () => {
+//     expect(divide(2,2)).toEqual(1);
+// });
+
+
+// test('multiply a * b  function', () => {
+//     expect(multiply(2,2)).toEqual(4);
+// });
+
+describe('calculator', () => {
+  test('should add two numbers correctly', () => {
+    expect(calculator.add(2, 3)).toEqual(5);
+  });
+
+  test('should subtract two numbers correctly', () => {
+    expect(calculator.subtract(5, 2)).toEqual(3);
+  });
+
+  test('should multiply two numbers correctly', () => {
+    expect(calculator.multiply(4, 6)).toEqual(24);
+  });
+
+  test('should divide two numbers correctly', () => {
+    expect(calculator.divide(10, 5)).toEqual(2);
+  });
+
+  test('should throw an error when dividing by zero', () => {
+    expect(() => calculator.divide(10, 0)).toThrowError();
+  });
 });
 
 
 // Shift the string "hello" by 3
-test('function that takes a string and returns it with each character shifted', () => {
-    expect(caesarCipher("hello", 3)).toBe("khoor");
-});
+// test('function that takes a string and returns it with each character shifted', () => {
+//     expect(caesarCipher("hello", 3)).toBe("khoor");
+// });
 
 
